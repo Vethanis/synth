f867fa6254a0975375597a6f83e5d8c64de15182
Lauren Hughes <lh0xfb@gmail.com>
2017-04-16 19:11:31 -0700

    pitch tracking

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <cstdio>
 #include <cstdlib>
+#include <cmath>
 #include <memory>
 #include <csignal>
 #include <vector>
@@ -20,47 +21,62 @@ enum MIDI_Action{
     NoteOff = 128
 };
 
+inline float midi2hz(unsigned char note){
+    float a = (note - 69) / 12.0f;
+    return 440.0f * powf(2.0f, a);
+}
+
+inline float hz2dphase(float hz){
+    return 2.0f * hz / float(sample_rate);
+}
+
 bool run = true;
 static void on_sigint(int signal){
     run = false;
 }
 
+struct midiud{
+    unsigned char note;
+};
+
 static void on_message(double timestamp, vector<unsigned char>* pmessage, void* userdata){
     if(!pmessage)
         return;
     auto& message = *pmessage;
+    midiud* ud = (midiud*)userdata;
     if(message.size() == 3){
         auto& action = message[0];
-        auto& note = message[1]; // C3 = 60
+        auto& note = message[1];
         auto& velocity = message[2];
         if(action == NoteOn){
-            printf("Note-on %i\n", note);
+            ud->note = note;
         }
         else if(action == NoteOff){
-            printf("Note-off %i\n", note);
         }
     }
 }
 
-struct aphase{
+struct audioud{
     float left, right;
+    unsigned char* note;
 };
 
 static int on_audio(const void* inbuf, void* outbuf, unsigned long num_frames, 
     const PaStreamCallbackTimeInfo* timeinfo, PaStreamCallbackFlags flags, void* userdata){
     
     float* output = (float*)outbuf;
-    aphase* phase = (aphase*)userdata;
+    audioud* phase = (audioud*)userdata;
     float& left = phase->left;
     float& right = phase->right;
+    float dphase = hz2dphase(midi2hz(phase->note[0]));
     for(unsigned i = 0; i < num_frames; i++){
         *output = left;
         output++;
         *output = right;
         output++;
 
-        left += 0.002f;
-        right += 0.004f;
+        left += dphase;
+        right += dphase;
         if(left > 1.0f)
             left -= 2.0f;
         if(right > 1.0f)
@@ -83,21 +99,23 @@ int main(){
     midiin->openPort(0);
     signal(SIGINT, on_sigint);
 
-    midiin->setCallback(on_message, nullptr);
+    midiud midiuserdata;
+    midiuserdata.note = 40;
+    midiin->setCallback(on_message, &midiuserdata);
 
     auto err = Pa_Initialize();
     tcpa(err)
 
     PaStream* stream = nullptr; 
-    aphase phase = {0.0f, 0.0f};
-    err = Pa_OpenDefaultStream(&stream, 0, 2, paFloat32, sample_rate, 256, on_audio, &phase);
+    audioud phase = {0.0f, 0.0f, &midiuserdata.note };
+    err = Pa_OpenDefaultStream(&stream, 0, 2, paFloat32, sample_rate, 32, on_audio, &phase);
     tcpa(err)
 
     err = Pa_StartStream(stream);
     tcpa(err)
 
     while(run){
-        this_thread::sleep_for(1s);
+        this_thread::sleep_for(0.1s);
     }
 
     err = Pa_StopStream(stream);
